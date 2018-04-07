e7b1e7ba7bd71f6318ba35410342d745c236445f
lsnq <pv@lsnq.ru>
2018-04-07 16:12:22 +0300

    01

--- algo-01.js
+++ algo-01.js
@@ -1,7 +1,8 @@
 const arr = [{name: 'a', value: 1}, {name: 'a', value: 1}]
 
 const arrToObj = arr.reduce((a,b) => {
-    a[b['name']] = b['value']
+    a[b['name']] = b['value'];
+    return a;
 }, {});
 
 console.log(arrToObj);
